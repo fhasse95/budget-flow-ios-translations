85290a6e13e04b4cb305c3106e4bfeb7d3318256
fhasse95 <49185957+fhasse95@users.noreply.github.com>
2026-03-13 19:19:29 +0100

    Moved localized strings to Swift package

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 6.2
+import PackageDescription
+
+let package = Package(
+    name: "budget-flow-translations",
+    defaultLocalization: "en",
+    platforms: [.iOS(.v16), .macOS(.v13), .watchOS(.v9)],
+    products: [
+        .library(name: "BudgetFlowTranslations", targets: ["BudgetFlowTranslations"])
+    ],
+    dependencies: [
+        .package(url: "https://source.skip.tools/skip.git", from: "1.7.4"),
+        .package(url: "https://source.skip.tools/skip-foundation.git", from: "1.3.13")
+    ],
+    targets: [
+        .target(
+            name: "BudgetFlowTranslations",
+            dependencies: [
+                .product(name: "SkipFoundation", package: "skip-foundation"),
+            ],
+            resources: [
+                .process("Resources")
+            ],
+            plugins: [
+                .plugin(name: "skipstone", package: "skip")
+            ]
+        )
+    ]
+)
---
+++ Sources/Library.swift
@@ -0,0 +1,13 @@
+//
+//  BudgetFlowTranslationsLibrary.swift
+//  BudgetFlowTranslations
+//
+//  Created by Fabian Hasse on 14.12.25.
+//  Copyright © 2025 Fabian Hasse. All rights reserved.
+//
+
+import Foundation
+
+public class BudgetFlowTranslationsLibrary {
+    public static let bundle: Bundle = Bundle.module
+}

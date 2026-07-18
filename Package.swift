4c997897a42290f9f27f322274de50e1413ceff6
fhasse95 <49185957+fhasse95@users.noreply.github.com>
2026-07-18 11:33:49 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,13 +4,13 @@ import PackageDescription
 let package = Package(
     name: "budget-flow-translations",
     defaultLocalization: "en",
-    platforms: [.iOS(.v16), .macOS(.v13), .watchOS(.v9)],
+    platforms: [.iOS(.v16), .macCatalyst(.v16), .macOS(.v13), .watchOS(.v9)],
     products: [
         .library(name: "BudgetFlowTranslations", targets: ["BudgetFlowTranslations"])
     ],
     dependencies: [
-        .package(url: "https://source.skip.tools/skip.git", from: "1.7.5"),
-        .package(url: "https://source.skip.tools/skip-foundation.git", from: "1.3.15")
+        .package(url: "https://source.skip.tools/skip.git", from: "1.9.4"),
+        .package(url: "https://source.skip.tools/skip-foundation.git", from: "1.4.2")
     ],
     targets: [
         .target(
